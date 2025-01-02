c93388d9c8e69955ddd4bfe23b219d0558349987
Zack Saidman <zachary.saidman@gmail.com>
2025-01-01 21:39:00 -0800

    Keep Image Aspect Ratio

--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,7 @@ const Home: React.FC = () => {
                     left: 0,
                     width: '100%',
                     height: '100%',
-                    objectFit: 'cover',
+                    objectFit: 'contain',
                     borderRadius: '10px',
                     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
                   }}
